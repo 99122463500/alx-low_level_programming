0d936191c2fd9a5ec9466624efced79782ca93e5
99122463500 <karabononyane99@gmail.com>
2023-11-23 12:38:47 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,32 +4,27 @@
 /**
  * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertible char
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int total = 0, power = 1;
-    int len = 0;
+	unsigned int total, power;
+	int len;
 
-    if (b == NULL)
-        return 0;
+	if (b == NULL)
+		return (0);
 
-    // Find the length of the string
-    while (b[len] != '\0')
-        len++;
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
 
-    // Check for invalid characters
-    for (int i = len - 1; i >= 0; i--)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return 0;
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
 
-        if (b[i] == '1')
-            total += power;
-
-        power *= 2;
-    }
-
-    return total;
+	return (total);
 }
-
