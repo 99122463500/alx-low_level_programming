127f6db923b049416a452c676ff25c09b893d5c6
99122463500 <karabononyane99@gmail.com>
2023-11-23 12:51:35 -0800

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,29 +1,11 @@
 #ifndef ALTERNATIVE_H
 #define ALTERNATIVE_H
-
-/* Custom putchar function */
-int custom_putchar(char c);
-
-/* Convert binary to unsigned int */
-unsigned int convert_binary_to_uint(const char *binary_string);
-
-/* Print binary representation of an unsigned long int */
-void display_binary(unsigned long int num);
-
-/* Get the value of a bit at a specific index */
-int obtain_bit_value(unsigned long int num, unsigned int index);
-
-/* Set a bit at a particular index to 1 */
-int set_bit_to_one(unsigned long int *num, unsigned int index);
-
-/* Set a bit at a particular index to 0 */
-int set_bit_to_zero(unsigned long int *num, unsigned int index);
-
-/* Count the number of bits to flip to transform one number to another */
-unsigned int count_flipped_bits(unsigned long int num1, unsigned long int num2);
-
-/* Check the endianness of the system */
-int check_endianness(void);
-
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
-
