21aaa89fe65ae460119a831bdb63f1616f7c8acc
99122463500 <karabononyane99@gmail.com>
2023-11-30 13:19:43 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
