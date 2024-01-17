cb93164a401ca251b3564ede3cd9d9a6cc2e2f61
99122463500 <karabononyane99@gmail.com>
2024-01-17 04:49:42 -0800

    100-atoi.c

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,46 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - a function that convert a string to an integer
  *
- * Return: the int converted from the string
- */
+ * @s: input string
+ *
+ * Return: integer
+*/
+
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	unsigned int No = 0;
+	int signof = 1;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	while (*s++)
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		if (No > 0)
+			break;
+		else if (*s == '-')
+			signof *= -1;
+		else if (*s >= '0' && *s <= '9')
+			No = (No * 10) + (*s - '0');
 	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (No * signof);
 }
